f5bb663ce8522d9afe71d2a15a97b6496ccf2129
Humza Munir <humza288@gmail.com>
2020-08-17 06:19:51 -0700

    basic asp.net and angularjs crud program

---
+++ Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularJSMvc.Models.EF;
+
+namespace AngularJSMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        private AngularsJSMvcDbContext db = null;
+        // GET: User
+
+        public UserController()
+        {
+            db = new AngularsJSMvcDbContext();
+        }
+        public ActionResult Index()
+        {
+            List<User> users = db.users.ToList();
+            return Json(users, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Details(int id)
+        {
+            var user = db.users.Find(id);
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Create(User user)
+        {
+            db.users.Add(user);
+            db.SaveChanges();
+            return Json(null);
+        }
+
+        [HttpPost]
+        public JsonResult Edit(User user)
+        {
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Json(null);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            var user = db.users.Find(id);
+            db.users.Remove(user);
+            db.SaveChanges();
+            return Json(null);
+        }
+    }
+}
---
+++ Models/EF/AngularsJSMvcDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSMvc.Models.EF
+{
+    public class AngularsJSMvcDbContext : DbContext
+    {
+        public AngularsJSMvcDbContext() : base("name = AngularJsMvcDbContext")
+        { 
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
